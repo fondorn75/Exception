4ae95317dfad09a616962d5bb2378d377b14df97
Добрин Дмитрий <zatoichi.cadzicu@gmail.com>
2022-09-15 17:17:21 +0300

    homework 1

--- homework_001.java
+++ homework_001.java
@@ -1,36 +1,35 @@
+// Создайте несколько методов, в каждом из которых, на этапе выполнения, JVM выбросит исключение.
+// 1. В методе создается целочисленная переменная, которая инициализируется значением 10 / 0
+// 2. В методе создается целочисленный массив длиной 10, и в ячейку этого массива с индексом 100 записывается значение 1
+// 3. В методе создается строка и инициализируется значением null, затем в консоль выводится длина этой строки
 
-class homework {
-    static int minArrayLength = 5;
-    static int[] firstArray = new int[] { 15, 25, 36 };
+class homework01 {
+    static int firstMetod() {
+        int result = 10 / 0;
 
-    /**
-     * @param array
-     * @return
-     */
-    static int getArrayLength(int[] array) {
-
-        if (array.length >= minArrayLength)
-            return array.length;
-        if (array.length == 0)
-            return -2;
-
-        return -1;
-
+        return result;
     }
 
-    /**
-     * @param args
-     */
+    static int[] secondMetod() {
+
+        int[] result = new int[10];
+
+        result[100] = 1;
+
+        return result;
+    }
+
+    static int thirdMetod() {
+        String temp = null;
+
+        int result = temp.length();
+
+        return result;
+    }
+
     public static void main(String[] args) {
-        int result = getArrayLength(firstArray);
-
-        if (result == -1)
-            System.out.println("Длина массива меньше " + minArrayLength);
-
-        if (result == -2)
-            System.out.println("Длина массива 0");
-
-        if (result >= minArrayLength)
-            System.out.println("Длина массива " + result);
+        // System.out.println(firstMetod());
+        // System.out.println(secondMetod());
+        System.out.println(thirdMetod());
     }
 }
