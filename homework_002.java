353edf33b253a3d76964e3ec7d8d6eb2edace45a
Добрин Дмитрий <zatoichi.cadzicu@gmail.com>
2022-09-16 11:46:47 +0300

    add task 4

--- homework_002.java
+++ homework_002.java
@@ -5,27 +5,52 @@
 
 public class homework_002 {
 
-    static String summTwoArrays(int[] array1, int[] array2) {
-        String result = "";
-        int temp = 0;
-        int tmp1 = array1.length;
-        int tmp2 = array2.length;
-        if (tmp1 != tmp2)
-            return "-1";
+    static int[] array1 = new int[] { 5, 4, 3, 8, 9, 6 };
+    static int[] array2 = new int[] { 4, 6, 5, 2, 3, 1 };
 
+    static int[] summTwoArrays(int[] array1, int[] array2) {
+        int[] result = new int[6];
         for (int index = 0; index < array1.length; index++) {
-            temp = array1[index] + array2[index];
-            result += (temp + " ");
+            result[index] = array1[index] - array2[index];
         }
 
         return result;
     }
 
-    static int[] array1 = new int[] { 5, 4, 3, 8, 9, 6 };
-    static int[] array2 = new int[] { 7, 2, 6, 3, 8, 2, 4 };
+    static String printArray(int[] array) {
+        String result = "";
+
+        for (int index = 0; index < array.length; index++) {
+            result += (array[index] + " ");
+        }
+
+        return result;
+    }
+
+    static int testExceptions(int[] array1, int[] array2) {
+        int tmp1 = array1.length;
+        int tmp2 = array2.length;
+
+        if (tmp1 != tmp2)
+            return -1;
+
+        return 1;
+    }
 
     public static void main(String[] args) {
-        System.out.println(summTwoArrays(array1, array2));
+
+        int[] temp = summTwoArrays(array1, array2);
+
+        switch (testExceptions(array1, array2)) {
+            case -1:
+                System.out.println("Массивы разной длины");
+                break;
+
+            default:
+                System.out.println("Операция может быть выполнена");
+                System.out.println(printArray(temp));
+                break;
+        }
 
     }
 }
--- homework_003.java
+++ homework_003.java
@@ -7,4 +7,60 @@
 
 public class homework_003 {
 
+    static int[] array1 = new int[] { 5, 4, 3, 8, 9, 6 };
+    static int[] array2 = new int[] { 4, 6, 5, 2, 3, 1 };
+
+    static float[] divideTwoArrays(int[] array1, int[] array2) {
+        float[] result = new float[6];
+        for (int index = 0; index < array1.length; index++) {
+
+            result[index] = array1[index] / array2[index];
+        }
+
+        return result;
+    }
+
+    static String printArray(float[] array) {
+        String result = "";
+
+        for (int index = 0; index < array.length; index++) {
+            result += (array[index] + " | ");
+        }
+
+        return result;
+    }
+
+    static int testExceptions(int[] array1, int[] array2) {
+        int tmp1 = array1.length;
+        int tmp2 = array2.length;
+        int temp = 0;
+        for (int index = 0; index < array2.length; index++) {
+            temp = array2[index];
+            if (temp == 0)
+                return -2;
+        }
+        if (tmp1 != tmp2)
+            return -1;
+
+        return 1;
+    }
+
+    public static void main(String[] args) {
+
+        switch (testExceptions(array1, array2)) {
+            case -1:
+                System.out.println("Массивы разной длины.");
+                break;
+            case -2:
+                System.out.println("В массиве есть 0. Деление на 0 не выполняется.");
+                break;
+
+            default:
+                System.out.println("Операция может быть выполнена.");
+                System.out.println(printArray(divideTwoArrays(array1, array2)));
+                break;
+        }
+
+    }
+
 }
